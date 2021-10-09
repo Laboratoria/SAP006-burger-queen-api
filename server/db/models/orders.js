78a08d836c0e86a61584f18bc8453ddece98fc95
Liziane Garcia <lizianegarciaa@gmail.com>
2021-10-08 23:50:37 -0300

    models e migrations criadas

---
+++ server/db/models/orders.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Orders extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Orders.init({
+    client_name: DataTypes.STRING,
+    user_id: DataTypes.INTEGER,
+    table: DataTypes.INTEGER,
+    status: DataTypes.STRING,
+    processedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Orders',
+  });
+  return Orders;
+};
