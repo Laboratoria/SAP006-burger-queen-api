1b6a06e4d88a93cf0f4a32587ca3eba5a06cdf79
Carmen Emilia Gozza <carmemilya1@gmail.com>
2021-10-28 11:40:44 -0300

    adicao dos produtos ao verbo getno controllers das orders

--- server/controller/orderController.js
+++ server/controller/orderController.js
@@ -1,68 +1,114 @@
 const { Orders, Products, ProductOrders} = require('../db/models');
 
     const getOrders = (req, res) => {
-        Orders.findAll()
+      Orders.findAll({
+        include: [
+          {
+            model: Products,
+            as: 'Products',
+            required: false,
+            attributes: [
+              'id',
+              'name',
+              'price',
+              'flavor',
+              'complement',
+              'image',
+              'type',
+              'sub_type',
+            ],
+            through: {
+              model: ProductOrders,
+              as: 'productOrders',
+              attributes: ['qtd'],
+            },
+          },
+        ],
+      })
         .then((result) => {
-        res.status(200).json(result);
+          res.status(200).json(result);
         })
-        .catch((error) => {
+        .catch((error) =>
           res.status(400).json({
-          code: 400,
-          error: error.message
+            code: 400,
+            error: error.message
           })
-      });
+        );
     };
     
     const postOrders = async (req, res) => {
-        const { user_id, client_name, table, status, processedAt } = req.body;
-        // cria o pedido
-        Orders.create({
-          client_name,
-          user_id, 
-          table,
-          status,
-          processedAt,
-       })
-       .then((result) => {
-         req.body.Products
-           .map((item) => {
-             const itemProduct = Products.findByPk(item.id);
-             if (!itemProduct) {
-               return res.status(400).json({
-                 message: "ERROR! Try again!",
-               });
-             }
-     
-             const itemOrders = {
-               order_id: result.id,
-               product_id: item.id,
-               qtd: item.qtd,
-             };
-     
-             ProductOrders.bulkCreate(itemOrders);
-     
-             return res.status(200).json(result);
-           })
-           .catch(() =>
-             res.status(400).json({
-               message: "ERROR! Try again!",
-             })
-           );
-       });
-     };
+      const { user_id, client_name, table, status, processedAt } = req.body;
+      await Orders.create({
+        client_name,
+        user_id, 
+        table,
+        status,
+        processedAt,
+      })
+      .then((result) => {
+        req.body.Products
+          .map((item) => {
+            const itemProduct = Products.findByPk(item.id);
+            if (!itemProduct) {
+              return res.status(400).json({
+                message: "ERROR! Try again!",
+              });
+            }
+    
+            const itemOrders = {
+              order_id: result.id,
+              product_id: item.id,
+              qtd: item.qtd,
+            };
+    
+            ProductOrders.bulkCreate(itemOrders);
+    
+            return res.status(200).json(result);
+          })
+          .catch(() =>
+            res.status(400).json({
+              message: "ERROR! Try again!",
+            })
+          );
+      });
+    };
    
 
     const getOrderId = async (req, res) => {
-      await Orders.findByPk(req.params.orderId)
-        .then((result) => {
+      await Orders.findAll({
+      where: { id: req.params.orderId },
+      include: [
+        {
+          model: Products,
+          as: 'Products',
+          required: false,
+          attributes: [
+            'id',
+            'name',
+            'price',
+            'flavor',
+            'complement',
+            'image',
+            'type',
+            'sub_type',
+          ],
+          through: {
+            model: ProductOrders,
+            as: 'productOrders',
+            attributes: ['qtd'],
+          },
+        },
+      ],
+    })
+      .then((result) => {
         res.status(200).json(result);
-        })
-        .catch((error) => {
-          res.status(400).json({
+      })
+      .catch((error) =>
+        res.status(400).json({
           code: 400,
           error: error.message
-          })
-      });
+        })
+      );
     }
 
     const putOrder = async (req, res) => {
